2951d983dcb8baae1252a102331ff2a2ac15525a
chYatima03 <53113121+chYatima03@users.noreply.github.com>
2024-01-15 11:56:23 +0700

    update

--- src/contents/header.js
+++ src/contents/header.js
@@ -2,7 +2,7 @@ import fileName from '../assets/resume.pdf'
 
 export const data = {
     name: "Yatima Chooruang",
-    title: "Software Developer · between jobs",
+    title: "Software Developer - Programmer · between jobs",
     caption: "From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.",
     btnText: "View Resume",
     link: fileName,
